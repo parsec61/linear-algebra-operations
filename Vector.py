e37039244c8f09229143fae37fe9d13bdf69dc83
parsec61 <philipgura@gmail.com>
2016-01-22 10:37:21 -0600

    Update Vector.py

--- Vector.py
+++ Vector.py
@@ -11,3 +11,10 @@ class Vector(object):
     
     except TypeError:
       raise ValueError('The coordinates must be an iterable')
+      
+  def __str__(self):
+    return 'Vector: {}'.format(self.coordinates)
+    
+  def __eq__(self, v):
+    return self.coordinates == v.coordinates
+    
